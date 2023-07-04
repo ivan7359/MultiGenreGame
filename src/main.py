88a7f30e28c5d0b0983b3cc0a2b4a68decda7fbc
Dmitry <dmitry@Dmitrys-MacBook-Pro.local>
2023-07-04 12:39:55 +0600

    Init commit

---
+++ src/main.py
@@ -0,0 +1,53 @@
+import pygame
+
+WIDTH, HEIGHT = 1280, 720
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.player_pos = pygame.Vector2(self.screen.get_width() / 2, self.screen.get_height() / 2)
+        self.running = True
+        self.speed = 300
+        self.dt = 0
+
+    def render(self):
+        self.screen.fill("black")
+        pygame.draw.circle(self.screen, "red", self.player_pos, 30)
+
+    def processInput(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.player_pos.y -= self.speed * self.dt
+        if keys[pygame.K_s]:
+            self.player_pos.y += self.speed * self.dt
+        if keys[pygame.K_a]:
+            self.player_pos.x -= self.speed * self.dt
+        if keys[pygame.K_d]:
+            self.player_pos.x += self.speed * self.dt
+
+    def update(self):
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        self.dt = self.clock.tick(60) / 1000
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.render()
+            self.processInput()
+            self.update()
+
+        pygame.quit()
+
+game = Game()
+game.run()
