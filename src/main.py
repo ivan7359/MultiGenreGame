a71dd6f19ff96808a3d38eb07135923cce0187f7
Ivan <ivan.homickii.com@gmail.com>
2023-07-18 22:05:53 +0900

    At main menu UI

--- src/main.py
+++ src/main.py
@@ -42,15 +42,21 @@ class Game():
         self.CurrPercent = 70
         self.img = pygame.image.load('media/pygame_logo_100x100.png')
 
-        self.hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((15, 15), (100, 50)), text='Say Hello', manager=self.manager)
-        self.slider = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((15, 75), (300, 25)), start_value=0, value_range=[0, 100], manager=self.manager)
-        self.text = pygame_gui.elements.UITextEntryBox(relative_rect=pygame.Rect((15, 115), (300, 300)), manager=self.manager)
-        self.menu = pygame_gui.elements.UIDropDownMenu(options_list=["1", "2", "3"], starting_option="1", relative_rect=pygame.Rect((15, 415), (100, 100)), manager=self.manager)
-        self.selectionList = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((15, 530), (100, 100)), item_list=["1", "2", "3"], manager=self.manager)
-        self.textEntryLine = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((15, 645), (300, 25)), manager=self.manager)
-        self.statusBar = pygame_gui.elements.UIStatusBar(relative_rect=pygame.Rect((15, 685), (300, 25)), manager=self.manager, percent_method=self.getCurrPercent)
-        self.label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((330, 15), (100, 25)), text="just a label", manager=self.manager)
-        self.image = pygame_gui.elements.UIImage(pygame.Rect((330, 50), (100, 100)), self.img, self.manager)
+        self.play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 4.41, HEIGHT / 2.93), (200, 70)), text='Play', manager=self.manager)
+        self.settings_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 4.41, HEIGHT / 1.93), (200, 70)), text='Settings', manager=self.manager)
+        self.exit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 4.41, HEIGHT / 1.43), (200, 70)), text='Exit', manager=self.manager)
+        self.info_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 42.67, HEIGHT / 24), (69, 69)), text='Info', manager=self.manager)
+        self.left_arrow_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 1.64, HEIGHT / 1.43), (94, 50)), text='Left arrow', manager=self.manager)
+        self.right_arrow_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((WIDTH / 1.38, HEIGHT / 1.43), (94, 50)), text='Right arrow', manager=self.manager)
+        self.label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((WIDTH / 3.9, HEIGHT / 7.2), (623, 70)), text="MultiGenreGame", manager=self.manager)
+        self.image = pygame_gui.elements.UIImage(pygame.Rect((WIDTH / 1.86, HEIGHT / 2.93), (427, 240)), self.img, self.manager)
+        
+        # self.slider = pygame_gui.elements.UIHorizontalSlider(relative_rect=pygame.Rect((15, 75), (300, 25)), start_value=0, value_range=[0, 100], manager=self.manager)
+        # self.text = pygame_gui.elements.UITextEntryBox(relative_rect=pygame.Rect((15, 115), (300, 300)), manager=self.manager)
+        # self.menu = pygame_gui.elements.UIDropDownMenu(options_list=["1", "2", "3"], starting_option="1", relative_rect=pygame.Rect((15, 415), (100, 100)), manager=self.manager)
+        # self.selectionList = pygame_gui.elements.UISelectionList(relative_rect=pygame.Rect((15, 530), (100, 100)), item_list=["1", "2", "3"], manager=self.manager)
+        # self.textEntryLine = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((15, 645), (300, 25)), manager=self.manager)
+        # self.statusBar = pygame_gui.elements.UIStatusBar(relative_rect=pygame.Rect((15, 685), (300, 25)), manager=self.manager, percent_method=self.getCurrPercent)
 ########################################################################
 
     # Singleton pattern
@@ -92,26 +98,36 @@ class Game():
 
 ############################## UI Events ###############################
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-              if event.ui_element == self.hello_button:
-                    print('Button was pressed!')
+              if event.ui_element == self.play_button:
+                    print('Button play was pressed!')
+              if event.ui_element == self.settings_button:
+                    print('Button settings was pressed!')
+              if event.ui_element == self.exit_button:
+                    print('Button exit was pressed!')
+              if event.ui_element == self.info_button:
+                    print('Button info was pressed!')
+              if event.ui_element == self.left_arrow_button:
+                    print('Button left arrow was pressed!')
+              if event.ui_element == self.right_arrow_button:
+                    print('Button right arrow was pressed!')
             
-            if event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
-              if event.ui_element == self.slider:
-                    print('current slider value:', event.value)
+            # if event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
+            #   if event.ui_element == self.slider:
+            #         print('current slider value:', event.value)
 
-            if event.type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
-                if event.ui_element == self.text:
-                    print("Changed text:", event.text)
+            # if event.type == pygame_gui.UI_TEXT_ENTRY_CHANGED:
+            #     if event.ui_element == self.text:
+            #         print("Changed text:", event.text)
 
-                if event.ui_element == self.textEntryLine:
-                    print("Changed textEntryLine:", event.text)
+            #     if event.ui_element == self.textEntryLine:
+            #         print("Changed textEntryLine:", event.text)
 
-            if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
-                print("Selected option:", event.text)
+            # if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+            #     print("Selected option:", event.text)
 
-            if event.type == pygame_gui.UI_SELECTION_LIST_NEW_SELECTION:
-                if event.ui_element == self.selectionList:
-                    print("Selected item:", event.text)
+            # if event.type == pygame_gui.UI_SELECTION_LIST_NEW_SELECTION:
+            #     if event.ui_element == self.selectionList:
+            #         print("Selected item:", event.text)
 
             self.manager.process_events(event)
 ########################################################################
