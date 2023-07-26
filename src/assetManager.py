575f4cd4587cab6c18c8602ede9bee9ae72e5cb1
Ivan <ivan.homickii.com@gmail.com>
2023-07-26 23:17:28 +0900

    Add shooter map

--- src/assetManager.py
+++ src/assetManager.py
@@ -10,22 +10,23 @@ class AssetManager():
         self.__result = []
         self.path = path
 
-    def __scanFolder(self, path, fileformat):
+    def __scanFolder(self, path, fileformats):
         listdir = os.listdir(path=path)
 
         for entity in listdir:
             if(os.path.isdir(path + '/' + entity)):
-                self.__scanFolder(path + '/' + entity, fileformat)
+                self.__scanFolder(path + '/' + entity, fileformats)
 
             if(os.path.isfile(path + '/' + entity)):
-                if(entity.find(fileformat) > 0):
-                    self.__result.append(path + '/' + entity)
+                for fileformat in fileformats:
+                    if(entity.find(fileformat) > 0):
+                        self.__result.append(path + '/' + entity)
 
     def __loadResources(self, type):
         if(type == 'images'):
-            self.__scanFolder(self.path, '.png')
+            self.__scanFolder(self.path, ['.png'])
         if(type == 'sounds'):
-            self.__scanFolder(self.path, '.mp3')
+            self.__scanFolder(self.path, ['.mp3', '.MP3'])
 
     def loadSounds(self):
         logging.info("LOADING SOUNDS")
@@ -34,6 +35,7 @@ class AssetManager():
         for file in self.__result:
             tmp = file.split('/')
             name = tmp[len(tmp) - 1].split('.')[0]
+            print(file)
             self.__sounds[name] = pygame.mixer.Sound(file)
             # self.__sounds[name] = file
 
