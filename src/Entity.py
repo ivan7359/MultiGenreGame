b50d63d675d8853d5e85f485547a92a4b53b1312
dmitri1998171 <dmitri1998171@icloud.com>
2023-08-11 17:08:49 +0600

    add config file for enemies

--- src/Entity.py
+++ src/Entity.py
@@ -2,15 +2,13 @@ import pygame
 from config import *
 
 class Entity:
-    def __init__(self, objSprites, publisher, pos= (0, 0)):
+    def __init__(self, objSprites, publisher, metrics, pos= (0, 0)):
         self.id = 0
         self.display_surface = pygame.display.get_surface()
         self.objSprites = objSprites
         self.publisher = publisher
 
         self.life = True
-        # self.health = 100
-        # self.damage = 30
 
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.image.fill('grey')
@@ -19,6 +17,9 @@ class Entity:
         self.speed = 8
         self.direction = pygame.math.Vector2()
         self.isMoving = [False, False]
+
+        self.health = metrics['health']
+        self.damage = metrics['damage']
 
     def setPos(self, x, y):
         self.rect.x = x
